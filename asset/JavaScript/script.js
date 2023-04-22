30f3783e185736b1e4dd8c1a79654047a2e06584
AstGon <ast.gonzalez@duocuc.cl>
2023-04-22 09:27:19 -0400

    actualizacion de java

--- asset/JavaScript/script.js
+++ asset/JavaScript/script.js
@@ -1,22 +1,59 @@
-(function () {
-  "use strict";
+const form = document.querySelector(".needs-validation");
 
-  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-  var forms = document.querySelectorAll(".needs-validation");
+form.addEventListener("submit", function (event) {
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  form.classList.add("was-validated");
+});
 
-  // Bucle sobre ellos y evitar el envío
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+const nombreInput = document.getElementById("nombre");
+const apellidoInput = document.getElementById("apellido");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("pwd");
+const checkInput = document.getElementById("myCheck");
 
-        form.classList.add("was-validated");
-      },
-      false
+nombreInput.addEventListener("input", function () {
+  if (nombreInput.value.length === 0) {
+    nombreInput.setCustomValidity("Por favor ingresar el nombre");
+  } else {
+    nombreInput.setCustomValidity("");
+  }
+});
+
+apellidoInput.addEventListener("input", function () {
+  if (apellidoInput.value.length === 0) {
+    apellidoInput.setCustomValidity("Por favor ingresar el apellido");
+  } else {
+    apellidoInput.setCustomValidity("");
+  }
+});
+
+emailInput.addEventListener("input", function () {
+  if (emailInput.validity.typeMismatch) {
+    emailInput.setCustomValidity(
+      "Por favor ingresar un correo electrónico válido"
     );
-  });
-})();
+  } else {
+    emailInput.setCustomValidity("");
+  }
+});
+
+passwordInput.addEventListener("input", function () {
+  if (passwordInput.validity.patternMismatch) {
+    passwordInput.setCustomValidity(
+      "La contraseña debe tener 8 caracteres, mayuscula, minuscula y números"
+    );
+  } else {
+    passwordInput.setCustomValidity("");
+  }
+});
+
+checkInput.addEventListener("click", function () {
+  if (checkInput.checked) {
+    checkInput.setCustomValidity("");
+  } else {
+    checkInput.setCustomValidity("Haz click para continuar");
+  }
+});
