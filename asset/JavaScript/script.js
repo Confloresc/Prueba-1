653d8f435f0ac34696acae84cd7f3d72de96d8bf
AstGon <ast.gonzalez@duocuc.cl>
2023-05-19 15:29:34 -0400

    Cambios

--- asset/JavaScript/script.js
+++ asset/JavaScript/script.js
@@ -1,3 +1,13 @@
+if (window.jQuery) {
+  // jQuery está vinculado correctamente
+  console.log("jQuery está vinculado correctamente en esta página.");
+} else {
+  // jQuery no está vinculado o no se ha cargado correctamente
+  console.log(
+    "jQuery no está vinculado o no se ha cargado correctamente en esta página."
+  );
+}
+
 const form = document.querySelector(".needs-validation");
 
 form.addEventListener("submit", function (event) {
